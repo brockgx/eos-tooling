770c01c6230c5c37db1365f33d4678f61bae7f05
MYRON TSALDARIS <100909217@student.swin.edu.au>
2021-08-04 17:03:55 +1000

    Added configuration data to Consul

--- terraform/templates/eos-app/networking/variables.tf
+++ terraform/templates/eos-app/networking/variables.tf
@@ -7,19 +7,21 @@ variable "region" {
   default = "us-east-1"
 }
 
-# No. of subnets to be created
-variable "subnet_count" {
-  default = 2
+# Consul server info 
+variable "consul_address" {
+  type        = string
+  description = "Address of Consul server"
+  default     = "127.0.0.1"
 }
 
-variable "cidr_block" {
-  default = "10.0.0.0/16"
+variable "consul_port" {
+  type        = number
+  description = "Port Consul server is listening on"
+  default     = "8500"
 }
 
-variable "private_subnets" {
-  type = list(any)
-}
-
-variable "public_subnets" {
-  type = list(any)
+variable "consul_datacenter" {
+  type        = string
+  description = "Name of the Consul datacenter"
+  default     = "dc1"
 }
